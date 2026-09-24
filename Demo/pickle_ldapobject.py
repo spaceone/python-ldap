1e84233e92658f647ba39d99bd6ba7d408db1fbe
Florian Best <ldap@freeiam.org>
2026-09-24 19:14:36 +0200

    style: fix PEP8 issues
    `ruff check --statistics --unsafe-fixes --preview --extend-select E --fixable E,I --fix`
    
    E231 missing-whitespace
    E111 indentation-with-invalid-multiple
    E225 missing-whitespace-around-operator
    E741 ambiguous-variable-name
    E226 missing-whitespace-around-arithmetic-operator
    E303 too-many-blank-lines
    E114 indentation-with-invalid-multiple-comment
    E265 no-space-after-block-comment
    E251 unexpected-spaces-around-keyword-parameter-equals
    E261 too-few-spaces-before-inline-comment
    E501 line-too-long
    E242 tab-after-comma
    E402 module-import-not-at-top-of-file
    E305 blank-lines-after-function-or-class
    E203 whitespace-before-punctuation
    E201 whitespace-after-open-bracket
    E711 none-comparison
    E302 blank-lines-top-level
    E202 whitespace-before-close-bracket
    E101 mixed-spaces-and-tabs
    E502 redundant-backslash
    E722 bare-except
    E713 not-in-test
    E714 not-is-test
    E221 multiple-spaces-before-operator
    E262 no-space-after-inline-comment
    E227 missing-whitespace-around-bitwise-or-shift-operator
    E241 multiple-spaces-after-comma
    E117 over-indented
    E703 useless-semicolon
    
    Issue: #657

--- Demo/pickle_ldapobject.py
+++ Demo/pickle_ldapobject.py
@@ -5,15 +5,15 @@ import pickle
 import ldap
 
 
-temp_file_name = os.path.join(os.environ.get('TMP','/tmp'),'pickle_ldap-%d' % (os.getpid()))
+temp_file_name = os.path.join(os.environ.get('TMP', '/tmp'), 'pickle_ldap-%d' % (os.getpid()))
 
-l1 = ldap.ldapobject.ReconnectLDAPObject('ldap://localhost:1390',trace_level=1)
+l1 = ldap.ldapobject.ReconnectLDAPObject('ldap://localhost:1390', trace_level=1)
 l1.protocol_version = 3
-l1.search_s('',ldap.SCOPE_BASE,'(objectClass=*)')
+l1.search_s('', ldap.SCOPE_BASE, '(objectClass=*)')
 
 with open(temp_file_name, 'wb') as pickle_file:
     pickle.dump(l1, pickle_file)
 
 with open(temp_file_name, 'rb') as pickle_file:
     l2 = pickle.load(pickle_file)
-l2.search_s('',ldap.SCOPE_BASE,'(objectClass=*)')
+l2.search_s('', ldap.SCOPE_BASE, '(objectClass=*)')
